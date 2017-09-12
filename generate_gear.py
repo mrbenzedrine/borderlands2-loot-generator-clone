8f40c98e99de64f5098d1f194c8e6cbe8ba745a4
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-09-12 04:02:45 +0100

    Remove comment clutter in generate_gear.py and gun.py

--- generate_gear.py
+++ generate_gear.py
@@ -8,32 +8,6 @@ import shotgun
 import sniper_rifle
 import launcher
 
-# Next cool thing would be to have different types of enemies which
-# then call generate_weapon() depending on their badass ranking,
-# ie, have super badasses drop a certain numebr of whatever guns
-
-# Oh, and also start doing shields, grenades etc
-
-# Could introduce the naming convention when I'm feeling like not
-# thinking too hard =P
-
-# Also, I'm gonna have to think about how to introduce the 
-# limitations of the gun depending on the manufacturer; ie,
-# torgue weapons are only explosive, the bandit manufacturer
-# doesn't manufacture sniper rifles etc
-# Perhaps you first generate the manufacturer of the parts BEFORE
-# you generate the weapon type, and then there are separate function
-# for generating bandit, dahl weapons etc because then there'll
-# be differences in the number of choices you have for WHAT weapon
-# types they can be and also WHICH weapon types are available for 
-# each manufacturer
-
-# Another limitation is that the manufacturer can be E-Tech only if
-# the gun is Purple rarity I think right?
-
-# Dunno how to handle E-Tech weapons yet since I don;t understand
-# how they work =P
-
 def generate_weapon(rarity, level):
 
     # rarity: string telling us what the rarity of the generated 
@@ -42,22 +16,6 @@ def generate_weapon(rarity, level):
     # level: integer; either the level of the enemy that dropped
     # the item or the level of the area in which the chest that
     # the item was found in was
-
-    # Need to generate the following things:
-
-    # The weapon type, ie, pistol, smg etc, and then, based on that:
-    
-    # 4 manufacturers; 1 for each different part of the weapon
-
-    # The level for the weapon, somehow based on the given info
-    # about the level of the enemy / area the weaon was dropped
-
-    # The element of the gun
-
-
-
-    # There are 6 weapon types, so choose a random number between
-    # 0 and 5
 
     weapon_type = choose_weapon_type()
 
--- gun.py
+++ gun.py
@@ -1,32 +1,3 @@
-# I reckon a gun's stats should be calculatable from the
-# different parts that make up the gun, so when setting
-# the stats of the gun, each stat could potentially
-# be the value of a method of the Gun class that calculates
-# the stat based on the parts that the gun has?
-
-# In order to generate a gun, you need a way to randomly 
-# pick the parts that a Gun object needs, randomly choose
-# an element and a weapon type, and since I saw that blog
-# post from the devloeprs that said in Brderlands 2 you
-# can specify the rairty of a wepaon drop, you explicitly
-# PICK a rarity instead of randomising it like the other
-# things
-
-# Stats depend on rarity, level, and the manufacturer of
-# the different parts of the gun, so perhaps there should
-# be base stats for each gun type starting at level 1,
-# then multplicative modifiers for the level of the gun, 
-# the rarity level and also the different manufacturers 
-# of the parts of the gun
-
-# The mutiplier modifer for the level of a gun should be a
-# function where you put into it the level and you get
-# an output which is the modifier I reckon, I think I
-# remember reading in that blog post that BL2 had an
-# exponential increase in enemy health so I think the 
-# weapons should also have the same increase otherwise the
-# weapon damage wouldn't scale correctly?
-
 from weapons_lvl1_base_stats import weapons_lvl1_base_stats
 import weapons_stats_modifiers
 
@@ -51,11 +22,6 @@ class Gun:
         # level: integer that tells us the level of the enemy that
         # dropped the gun / the level of the area in which the chest
         # that the gun was found in
-
-        # I think it's best to generate the Title of the gun somewhere
-        # in this initialisation function
-
-        # print(weapons_stats_modifiers.rarity_modifiers)
 
         self.type = type
         self.parts = parts
