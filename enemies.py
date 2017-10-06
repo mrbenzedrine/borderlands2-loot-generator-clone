fd0128b1d01adbaaef41d43e8ecf50f1a3895ff3
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-10-06 18:21:11 +0100

    Add more enemy types to enemies.py

--- enemies.py
+++ enemies.py
@@ -15,3 +15,115 @@ class Chump:
                 'quantity': 1
             }
         )
+
+class Badass:
+
+    def __init__(self, level):
+
+        self.level = level
+        self.loot_pool = (
+            {
+                'rarity': 'White',
+                'quantity': 2
+            },
+            {
+                'rarity': 'Green',
+                'quantity': 2
+            }
+        )
+
+class SuperBadass():
+
+    def __init__(self, level):
+
+        self.level = level
+        self.loot_pool = (
+            {
+                'rarity': 'White',
+                'quantity': 2
+            },
+            {
+                'rarity': 'Green',
+                'quantity': 2
+            },
+            {
+                'rarity': 'Blue',
+                'quantity': 1
+            }
+        )
+
+class UltimateBadass():
+
+    def __init__(self, level):
+
+        self.level = level
+        self.loot_pool = (
+            {
+                'rarity': 'White',
+                'quantity': 2
+            },
+            {
+                'rarity': 'Green',
+                'quantity': 2
+            },
+            {
+                'rarity': 'Blue',
+                'quantity': 2
+            },
+            {
+                'rarity': 'Purple',
+                'quantity': 1
+            }
+        )
+
+class Chubby():
+
+    def __init__(self, level):
+
+        self.level = level
+        self.loot_pool = (
+            {
+                'rarity': 'Green',
+                'quantity': 2
+            },
+            {
+                'rarity': 'Blue',
+                'quantity': 1
+            },
+            {
+                'rarity': 'Purple',
+                'quantity': 1
+            },
+            {
+                'rarity': 'Orange',
+                'quantity': 1
+            }
+        )
+
+class RaidBoss():
+
+    def __init__(self, level):
+
+        self.level = level
+        self.loot_pool = (
+            {
+                'rarity': 'White',
+                'quantity': 3
+            },
+            {
+                'rarity': 'Green',
+                'quantity': 3
+            },
+            {
+                'rarity': 'Blue',
+                'quantity': 3
+            },
+            {
+                'rarity': 'Purple',
+                'quantity': 2
+            },
+            {
+                'rarity': 'Orange',
+                'quantity': 1
+            }        
+        )
