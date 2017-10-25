2ed546be0bcd15286ef39fdbe398b155a60a8eb5
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-10-25 17:57:21 +0100

    Put some code from infiltrator.py into a separate file for reuse

---
+++ gear/class_mods/general_class_mod_functions.py
@@ -0,0 +1,31 @@
+import random
+
+def get_remaining_skills_to_boost(rarity, com_prefix, fixed_skills_from_com_to_boost, all_skills_to_boost):
+    
+    leftover_fixed_skills = []
+
+    for prefix in fixed_skills_from_com_to_boost:
+        if(prefix != com_prefix):
+            leftover_fixed_skills.append(fixed_skills_from_com_to_boost[prefix])
+
+    other_skills_to_boost = []
+
+    if(rarity == 'Blue'):
+        # Get 1 extra skill
+
+        random_integer = random.randint(0,1)
+        other_skills_to_boost.append(leftover_fixed_skills[random_integer])
+
+    else:
+        # Purple rarity, so get 2 extra skills, and these will just be the
+        # 2 skills in leftover_fixed_skills
+
+        other_skills_to_boost = leftover_fixed_skills
+
+    # Add the skills in other_skills_to_boost to all_skills_to_boost
+
+    for x in range(0, len(other_skills_to_boost)):
+        # Use a placeholder value of 1 for now
+        all_skills_to_boost[other_skills_to_boost[x]] = 1
+
+    return all_skills_to_boost
