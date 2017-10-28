3c0372d2f918675ec674e5767b741fd4ee55ac4e
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-10-28 02:32:15 +0100

    Add other assassin class mod types to assassin_class_mods_info.py

--- gear/class_mods/assassin_class_mods_info.py
+++ gear/class_mods/assassin_class_mods_info.py
@@ -10,8 +10,88 @@ assassin_class_mods = {
         }
     },
 
+    'killer': {
+        'prefixes': ['Unpredictable', 'The', 'Contemptible'],
+        'stat_changes': ['Team critical hit damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Unpredictable': 'Fearless',
+            'The': 'Killer',
+            'Contemptible': 'Ambush'
+        }
+    },
+
+    'ninja': {
+        'prefixes': ['Shadow', 'Bloodied', 'Lethal'],
+        'stat_changes': ['Melee damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Shadow': 'Backstab',
+            'Bloodied': 'Resurgence',
+            'Lethal': 'Killing Bl0w'
+        }
+    },
+
+    'professional': {
+        'prefixes': ['Unequaled', 'The', 'Succinct'],
+        'stat_changes': ['Reload speed', 'Shield capacity'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Unequaled': 'Tw0 Fang',
+            'The': 'Killer',
+            'Succinct': 'Ambush'
+        }
+    },
+
     'rogue': {
         'fixed_skills_to_boost': ['Like the Wind', 'Tw0 Fang', 'Vel0city']
+    },
+
+    'shot': {
+        'prefixes': ['Long', 'Sure', 'True'],
+        'stat_changes': ['Team accuracy'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Long': 'Optics',
+            'Sure': 'Precisi0n',
+            'True': 'Kill C0nfirmed'
+        }
+    },
+
+    'sniper': {
+        'prefixes': ['Tranquil', 'One Shot', 'Deadly'],
+        'stat_changes': ['Sniper rifle damage', 'Sniper rifle critical damage', 'Shield capacity'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Tranquil': 'Precisi0n',
+            'One Shot': '0ne Sh0t 0ne Kill',
+            'Deadly': 'Headsh0t'
+        }
+    },
+
+    'spy': {
+        'prefixes': ['Swift', 'Contemptible', 'Tricky'],
+        'stat_changes': ['Action skill cooldown rate'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Swift': 'Vel0city',
+            'Contemptible': 'Ambush',
+            'Tricky': 'Unf0rseen'
+        }
+    },
+
+    'stalker': {
+        'prefixes': ['Shadow', 'Disturbed', 'Razored'],
+        'stat_changes': ['Pistol fire rate', 'Pistol magazine size', 'Shield recharge rate'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Shadow': 'F0ll0wthr0ugh',
+            'Disturbed': 'Rising Sh0t',
+            'Razored': 'C0unter Strike'
+        }
+    },
+
+    'survivor': {
+        'prefixes': ['Disturbed', 'Bloodied', 'Contemptible'],
+        'stat_changes': ['Max health', 'Health regeneration'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Disturbed': 'Rising Sh0t',
+            'Bloodied': 'Innervate',
+            'Contemptible': 'Ambush'
+        }
     }
 
 }
