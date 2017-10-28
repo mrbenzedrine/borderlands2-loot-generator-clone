ade38fc508ee5220244eb40c729f8fcdbdab5919
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-10-29 00:17:34 +0100

    Add data to commando_class_mods_info.py

--- gear/class_mods/commando_class_mods_info.py
+++ gear/class_mods/commando_class_mods_info.py
@@ -1,3 +1,97 @@
 commando_class_mods = {
     
+    'engineer': {
+        'prefixes': ['Sentry', 'Front Line', 'Efficient'],
+        'stat_changes': ['Action skill cooldown rate'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Sentry': 'Sentry',
+            'Front Line': 'Battlefront',
+            'Efficient': 'Resourceful'
+        }
+    },
+
+    'grenadier': {
+        'prefixes': ['Master', 'Impact', 'Expert'],
+        'stat_changes': ['Grenade damage', 'Explosive damage resistance'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Master': 'Grenadier',
+            'Impact': 'Impact',
+            'Expert': 'Steady'
+        }
+    },
+
+    'gunner': {
+        'prefixes': ['Support', 'Heavy', 'Auto'],
+        'stat_changes': ['Team fire rate'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Support': 'Overload',
+            'Heavy': 'Metal Storm',
+            'Auto': 'Steady'
+        }
+    },
+
+    'pointman': {
+        'prefixes': ['Grisly', 'Juiced', 'Unstoppable'],
+        'stat_changes': ['Health regenration', 'Max health'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Grisly': 'Last Ditch Effort',
+            'Juiced': 'Pressure',
+            'Unstoppable': 'Grit'
+        }
+    },
+
+    'ranger': {
+        'fixed_skills_to_boost': ['Impact', 'Ranger', 'Grit']
+    },
+
+    'rifleman': {
+        'prefixes': ['Assault', 'Impact', 'Front Line'],
+        'stat_changes': ['Assault rifle magazine size', 'Assault rifle gun damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Assault': 'Onslaught',
+            'Impact': 'Impact',
+            'Front Line': 'Battlefront'
+        }
+    },
+
+    'shock trooper': {
+        'prefixes': ['Assault', 'Heavy', 'Security'],
+        'stat_changes': ['Shock damage resistance', 'Electrocute damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Assault': 'Onslaught',
+            'Heavy': 'Metal Storm',
+            'Security': 'Quick Charge'
+        }
+    },
+
+    'specialist': {
+        'prefixes': ['Warfare', 'Military', 'Hazard'],
+        'stat_changes': ['SMG accuracy'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Warfare': 'Ready',
+            'Military': 'Duty Calls',
+            'Hazard': 'Forbearance'
+        }
+    },
+
+    'tactician': {
+        'prefixes': ['Front Line', 'Determined', 'Efficient'],
+        'stat_changes': ['Team shield recharge rate', 'Team shield recharge delay'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Front Line': 'Battlefront',
+            'Determined': 'Preparation',
+            'Efficient': 'Resourceful'
+        }
+    },
+
+    'veteran': {
+        'prefixes': ['Expert', 'Die Hard', 'Juiced'],
+        'stat_changes': ['Relaod speed', 'Magazine size'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Expert': 'Steady',
+            'Die Hard': 'Last Ditch Effort',
+            'Juiced': 'Pressure'
+        }
+    }
+
 }
