b109328e99e3d80b0d16ffb9108c02ce04418dc1
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-10-29 17:55:35 +0100

    Add data to gunzerker_class_mods_info.py

--- gear/class_mods/gunzerker_class_mods_info.py
+++ gear/class_mods/gunzerker_class_mods_info.py
@@ -1,3 +1,97 @@
 gunzerker_class_mods = {
     
+    'beast': {
+        'prefixes': ['Savage', 'Vengeful', 'Altered'],
+        'stat_changes': ['Max health', 'Reload speed'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Savage': 'Just Got Real',
+            'Vengeful': 'Incite',
+            'Altered': 'Sexual Tyrannosaurus'
+        }
+    },
+
+    'bezerker': {
+        'prefixes': ['Unhinged', 'Two Gun', 'Unstoppable'],
+        'stat_changes': ['Action skill cooldown rate'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Unhinged': 'Lay Waste',
+            'Two Gun': 'Last Longer',
+            'Unstoppable': 'Yippee Ki Yay'
+        }
+    },
+
+    'devastator': {
+        'prefixes': ['Hoarding', 'Savage', 'Pragmatic'],
+        'stat_changes': ['Team magazine size'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Hoarding': 'Filled to the Brim',
+            'Savage': 'Just Got Real',
+            'Pragmatic': 'Divergent Likeness'
+        }
+    },
+
+    'monk': {
+        'fixed_skills_to_boost': ['Money Shot', 'Sexual Tyrannosaurus', 'Asbestos']
+    },
+
+    'hoarder': {
+        'prefixes': ['Unhinged', 'Lucky', 'Loaded'],
+        'stat_changes': ['Team bullet regeneration'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Unhinged': 'Lay Waste',
+            'Lucky': '5 Shots or 6',
+            'Loaded': 'Filled to the Brim'
+        }
+    },
+
+    'raider': {
+        'prefixes': ['Auspicious', 'Cocky', 'Vengeful'],
+        'stat_changes': ['Assault rifle accuracy', 'Shield capacity'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Auspicious': 'Money Shot',
+            'Cocky': 'Locked and Loaded',
+            'Vengeful': 'Insight'
+        }
+    },
+
+    'renegade': {
+        'prefixes': ['Suave', 'Vengeful', 'Cocky'],
+        'stat_changes': ['Pistol gun damage', 'Pistol accuracy'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Suave': 'I\'m Your Huckleberry',
+            'Vengeful': 'Incite',
+            'Cocky': 'All I Need is One'
+        }
+    },
+
+    'tank': {
+        'prefixes': ['Armored', 'Hulking', 'Resistance'],
+        'stat_changes': ['Shield capacity', 'Shield recharge rate'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Armored': 'I\'m the Juggernaut',
+            'Hulking': 'Hard to Kill',
+            'Resistance': 'Asbestos'
+        }
+    },
+
+    'titan': {
+        'prefixes': ['Hulking', 'Grand', 'Armored'],
+        'stat_changes': ['Health regeneration', 'Gun damage', 'Shield capacity'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Hulking': 'Hard to Kill',
+            'Grand': 'Out of Bubblegum',
+            'Armored': 'I\'m the Juggernaut'
+        }
+    },
+
+    'war dog': {
+        'prefixes': ['Lucky', 'Determined', 'Hoarding'],
+        'stat_changes': ['Reload speed', 'Magazine size'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Lucky': '5 Shot or 6',
+            'Determined': 'Out of Bubblegum',
+            'Hoarding': 'Filled to the Brim'
+        }
+    }
+
 }
