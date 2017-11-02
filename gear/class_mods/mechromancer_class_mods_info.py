a90d77959791a2bff27a418227708c74a9c11473
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-11-02 16:23:58 +0100

    Add data to mechromancer_class_mods_info.py

--- gear/class_mods/mechromancer_class_mods_info.py
+++ gear/class_mods/mechromancer_class_mods_info.py
@@ -1,3 +1,97 @@
 mechromancer_class_mods = {
-    
+
+    'anarchist': {
+        'prefixes': ['Calculating', 'Airborne', 'Swift'],
+        'stat_changes': ['Gun damage', 'Gun accuracy'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Calculating': 'Fancy Mathematics',
+            'Airborne': 'Death from Above',
+            'Swift': 'Annoyed Android'
+        }
+    },
+
+    'catalyst': {
+        'prefixes': ['Ionized', 'Evil', 'Wired'],
+        'stat_changes': ['Team elemental effect damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Ionized': 'Shock Storm',
+            'Evil': 'Evil Enchantress',
+            'Wired': 'Wires Don\'t Talk'
+        }
+    },
+
+    'jill of all trades': {
+        'prefixes': ['Improved', 'Peppy', 'Speedy'],
+        'stat_changes': ['Assault rifle damage', 'Assault rifle magazine size'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Improved': 'The Better Half',
+            'Peppy': 'More Pep',
+            'Speedy': 'Smaller, Lighter, Faster'
+        }
+    },
+
+    'necromancer': {
+        'fixed_skills_to_boost': ['Made of Sterner Stuff', 'Strength of Five Gorillas', 'Wires Don\'t Talk']
+    },
+
+    'prodigy': {
+        'prefixes': ['Patient', 'Superior', 'Peppy'],
+        'stat_changes': ['Weapon accuracy', 'Reload speed'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Patient': 'Cooking Up Trouble',
+            'Superior': 'The Better Half',
+            'Peppy': 'More Pep'
+        }
+    },
+
+    'punk': {
+        'prefixes': ['Superior', 'Paranoid', 'Speedy'],
+        'stat_changes': ['Fire rate', 'Magazine size'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Superior': 'The Better Half',
+            'Paranoid': 'Annoyed Android',
+            'Speedy': 'Smaller, Lighter, Faster'
+        }
+    },
+
+    'roboteer': {
+        'prefixes': ['Stern', 'Potent', 'Tenacious'],
+        'stat_changes': ['Action skill cooldown rate', 'Melee damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Stern': 'Made of Sterner Stuff',
+            'Potent': 'Potent as a Pony',
+            'Tenacious': 'Strength of Five Gorillas'
+        }
+    },
+
+    'sweetheart': {
+        'prefixes': ['Patient', 'Superior', 'Peppy'],
+        'stat_changes': ['Team max health'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Patient': 'Cooking Up Trouble',
+            'Superior': 'The Better Half',
+            'Peppy': 'More Pep'
+        }
+    },
+
+    'technophile': {
+        'prefixes': ['Calculating', 'Tenacious', 'Manic'],
+        'stat_changes': ['Shield capacity', 'Shield recharge rate'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Calculating': 'Fancy Mathematics',
+            'Tenacious': 'Strength of Five Gorillas',
+            'Manic': 'Interspersed Outburst'
+        }
+    },
+
+    'zapper': {
+        'prefixes': ['Scorching', 'Peppy', 'Wired'],
+        'stat_changes': ['Shock damage', 'Chance to shock'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Scorching': 'Electrical Burn',
+            'Peppy': 'More Pep',
+            'Wired': 'Wires Don\'t Talk'
+        }
+    }
+
 }
