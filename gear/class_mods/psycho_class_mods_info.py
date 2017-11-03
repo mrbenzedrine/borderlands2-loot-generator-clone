daccc98f9648ec75b3352dd24e4057e628b324fd
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-11-03 15:45:14 +0100

    Add data to psycho_class_mods_info.py

--- gear/class_mods/psycho_class_mods_info.py
+++ gear/class_mods/psycho_class_mods_info.py
@@ -1,3 +1,97 @@
 psycho_class_mods = {
     
+    'barbarian': {
+        'fixed_skills_to_boost': ['Blood Trance', 'Salt the Wound', 'Fire Fiend']
+    },
+
+    'blister': {
+        'prefixes': ['Diesel', 'Driven', 'Blood'],
+        'stat_changes': ['Grenade capacity', 'Kill skill duration'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Diesel': 'Fuel the Blood',
+            'Driven': 'Blood Overdrive',
+            'Blood': 'Blood Bath'
+        }
+    },
+
+    'crunch': {
+        'prefixes': ['Tasty', 'Dazed', 'Flesh'],
+        'stat_changes': ['Explosive damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Tasty': 'Taste of Blood',
+            'Dazed': 'Blood Trance',
+            'Flesh': 'Strip the Flesh'
+        }
+    },
+
+    'meat': {
+        'prefixes': ['Rage', 'Pain', 'Salt'],
+        'stat_changes': ['Health regeneration', 'Shield recharge delay'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Rage': 'Empty the Rage',
+            'Pain': 'Embrace the Pain',
+            'Salt': 'Salt the Wound'
+        }
+    },
+
+    'reaper': {
+        'prefixes': ['Twitchy', 'Driven', 'Fuel'],
+        'stat_changes': ['Team kill skill duration'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Twitchy': 'Blood Twitch',
+            'Driven': 'Blood Overdrive',
+            'Fuel': 'Fuel the Fire'
+        }
+    },
+
+    'sickle': {
+        'prefixes': ['Diesel', 'Rage', 'Scream'],
+        'stat_changes': ['Melee damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Diesel': 'Fuel the Blood',
+            'Rage': 'Empty the Rage',
+            'Scream': 'Silence the Voices'
+        }
+    },
+
+    'slab': {
+        'prefixes': ['Gore', 'Rampaging', 'Beef'],
+        'stat_changes': ['Max health'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Gore': 'Thrill of the Kill',
+            'Rampaging': 'Fuel the Rampage',
+            'Beef': 'Feed the Meat'
+        }
+    },
+
+    'toast': {
+        'prefixes': ['Power', 'Burnt', 'Razed'],
+        'stat_changes': ['Burn chance', 'Magazine size'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Power': 'Pain is Power',
+            'Burnt': 'Fire Fiend',
+            'Razed': 'Elemental Elation'
+        }
+    },
+
+    'torch': {
+        'prefixes': ['Blaze', 'Fuel', 'Flare'],
+        'stat_changes': ['Burn damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Blaze': 'Burn, Baby. Burn',
+            'Fuel': 'Fuel the Fire',
+            'Flare': 'Flame Flare'
+        }
+    },
+
+    'wound': {
+        'prefixes': ['Twitchy', 'Tasty', 'Hot'],
+        'stat_changes': ['Fire rate after kill'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Twitchy': 'Blood Twitch',
+            'Tasty': 'Taste of Blood',
+            'Hot': 'Boiling Blood'
+        }
+    }
+
 }
