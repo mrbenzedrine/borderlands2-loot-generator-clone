0a94b3068e93493fca2e2691265a90f6c8238f18
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-11-03 16:17:43 +0100

    Add data to siren_class_mods_info.py

--- gear/class_mods/siren_class_mods_info.py
+++ gear/class_mods/siren_class_mods_info.py
@@ -1,3 +1,97 @@
 siren_class_mods = {
-    
+   
+    'banshee': {
+        'prefixes': ['Unyielding', 'Breakneck', 'Scorched'],
+        'stat_changes': ['Shield recharge rate', 'Shield recharge delay', 'Max health'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Unyielding': 'Immolate',
+            'Breakneck': 'Fleet',
+            'Scorched': 'Backdraft'
+        }
+    },
+
+    'binder': {
+        'prefixes': ['Hell', 'Cunning', 'Chrono'],
+        'stat_changes': ['Team action skill cooldown rate'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Hell': 'Helios',
+            'Cunning': 'Reaper',
+            'Chrono': 'Suspension'
+        }
+    },
+
+    'cat': {
+        'prefixes': ['Killer', 'Scheming', 'Wild'],
+        'stat_changes': ['SMG damage', 'SMG accuracy'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Killer': 'Mind\'s Eye',
+            'Scheming': 'Wreck',
+            'Wild': 'Accelerate'
+        }
+    },
+
+    'cleric': {
+        'fixed_skills_to_boost': ['Suspension', 'Life Tap', 'Restoration']
+    },
+
+    'fox': {
+        'prefixes': ['Hell', 'Cunning', 'Plague'],
+        'stat_changes': ['Burn chance', 'Burn damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Hell': 'Helios',
+            'Cunning': 'Reaper',
+            'Plague': 'Blight Phoenix'
+        }
+    },
+
+    'matriarch': {
+        'prefixes': ['Leeching', 'Relentless', 'Angelic'],
+        'stat_changes': ['Max health', 'Magazine size'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Leeching': 'Life Tap',
+            'Relentless': 'Sustenance',
+            'Angelic': 'Elated'
+        }
+    },
+
+    'nurse': {
+        'prefixes': ['Merciful', 'Angelic', 'Heavenly'],
+        'stat_changes': ['Team health regeneration'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Merciful': 'Restoration',
+            'Angelic': 'Elated',
+            'Heavenly': 'Sweet Release'
+        }
+    },
+
+    'trickster': {
+        'prefixes': ['Thieving', 'Mirrored', 'Blurred'],
+        'stat_changes': ['Fire rate'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Thieving': 'Life Tap',
+            'Mirrored': 'Kinetic Reflection',
+            'Blurred': 'Chain Reaction'
+        }
+    },
+
+    'warder': {
+        'prefixes': ['Scheming', 'Guarded', 'Rebounding'],
+        'stat_changes': ['Shield capacity'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Scheming': 'Wreck',
+            'Guarded': 'Ward',
+            'Rebounding': 'Recompense'
+        }
+    },
+
+    'witch': {
+        'prefixes': ['Hell', 'Splatter', 'Bullet'],
+        'stat_changes': ['Corrode chance', 'Corrode damage'],
+        'fixed_skills_to_boost_by_com_prefix': {
+            'Hell': 'Helios',
+            'Splatter': 'Flicker',
+            'Bullet': 'Kinetic Reflection'
+        }
+    }
+
 }
