e3266809e420e13eee1e339dc055277625465c28
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-11-29 18:35:44 +0000

    Add standard.py to generate the stats of a standard grenade

---
+++ gear/grenade_mods/standard.py
@@ -0,0 +1,33 @@
+from . import general_grenade_mod_functions 
+
+def generate(level, rarity):
+
+    main_stats = calculate_main_stats(level, rarity)
+
+    other_stats = {
+        'element': general_grenade_mod_functions.choose_element(),
+        'delivery_mechanism': general_grenade_mod_functions.choose_delivery_mechanism()
+    }
+
+    type_specific_stats = 'none'
+
+    stats = {
+        'main_stats': main_stats,
+        'other_stats': other_stats,
+        'type_specific_stats': type_specific_stats
+    }
+
+    return stats
+
+def calculate_main_stats(level, rarity):
+
+    # Use placeholder values of 0 for now, but will eventually use
+    # the level and rarity as factors in determining these values
+    
+    main_stats = {
+        'damage': 0,
+        'blast_radius': 0,
+        'fuse_time': 0
+    }
+
+    return main_stats
