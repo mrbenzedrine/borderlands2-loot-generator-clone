038bf5f896eb3e1b5a38e74718fc01680515e782
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-11-30 20:42:47 +0000

    Add amplify.py to generate the stats of an amplify shield

---
+++ gear/shields/amplify.py
@@ -0,0 +1,17 @@
+
+def calculate_main_stats(level, rarity):
+
+    main_stats = {
+        'capacity': 0,
+        'recharge_rate': 0,
+        'recharge_delay': 0
+    }
+
+    return main_stats
+
+def calculate_type_specific_stats(level, rarity):
+
+    return {
+        'amp_damage': 0,
+        'amp_shot_drain': 0
+    }
