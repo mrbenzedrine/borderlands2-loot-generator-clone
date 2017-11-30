999106d60e9666f5daf93774dd345d94ab3cd19b
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-11-30 22:28:09 +0000

    Add nova.py to generate the stats of a nova shield

---
+++ gear/shields/nova.py
@@ -0,0 +1,19 @@
+from . import general_shield_functions
+
+def calculate_main_stats(level, rarity):
+
+    main_stats = {
+        'capacity': 0,
+        'recharge_rate': 0,
+        'recharge_delay': 0
+    }
+
+    return main_stats
+
+def calculate_type_specific_stats(level, rarity):
+
+    return {
+        'element': general_shield_functions.choose_element(),
+        'nova_radius': 0,
+        'nova_damage': 0
+    }
