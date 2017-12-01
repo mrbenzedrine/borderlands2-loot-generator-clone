6f1dce5a4905b926567d6f0bcbd70e25ea3cc232
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-01 03:21:15 +0000

    Edit nova.py to check if the nova element and shield manufacturer are a valid combination

--- gear/shields/nova.py
+++ gear/shields/nova.py
@@ -10,10 +10,19 @@ def calculate_main_stats(level, rarity):
 
     return main_stats
 
-def calculate_type_specific_stats(level, rarity):
+def calculate_type_specific_stats(level, rarity, manufacturer):
+
+    if(manufacturer == 'Torgue'):
+        nova_element = 'Explosion'
+    else:
+        nova_element = general_shield_functions.choose_element()
+
+    # Check if the element and the manufacturer are a valid combination
+
+    valid_nova_element = general_shield_functions.get_valid_nova_spike_manufacturer_element_combo(manufacturer, nova_element, 'nova')
 
     return {
-        'element': general_shield_functions.choose_element(),
+        'element': valid_nova_element,
         'nova_radius': 0,
         'nova_damage': 0
     }
