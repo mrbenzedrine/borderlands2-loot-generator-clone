4a1b86cfb9d9ed63da51a841b99e5e7d5638ec85
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-14 18:54:34 +0000

    Edit general_relic_functions.py to include 'non-elemental' as an element type

--- gear/relics/elemental.py
+++ gear/relics/elemental.py
@@ -6,6 +6,15 @@ def calculate_stats(level, rarity):
     relic_element = general_relic_functions.choose_element()
     damage_increase = calculate_damage_increase(level, rarity)
 
+    # Can't have non-elemental elemental relic, so check if we have
+    # a valid element
+
+    while True:
+        if(check_valid_element(relic_element) is True):
+            break
+        else:
+            relic_element = general_relic_functions.choose_element()
+
     stats = {
         'element': relic_element,
         'damage increase': damage_increase
@@ -17,3 +26,8 @@ def calculate_damage_increase(level, rarity):
 
     return '+0%'
 
+def check_valid_element(element):
+
+    test = element != 'Non-elemental'
+
+    return test
--- gear/relics/general_relic_functions.py
+++ gear/relics/general_relic_functions.py
@@ -2,13 +2,14 @@ import random
 
 def choose_element():
 
-    random_integer = random.randint(0,4)
+    random_integer = random.randint(0,5)
     switcher = {
         0: 'Incendiary',
         1: 'Corrosion',
         2: 'Shock',
         3: 'Slag',
-        4: 'Explosion'
+        4: 'Explosion',
+        5: 'Non-elemental'
     }
     return switcher.get(random_integer, 'nothing') 
 
