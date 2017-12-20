dd4d7fdd0aadb0184960cc5ecdff8e9bfca940d0
Yousef Moazzam <mrbenzedrine@live.co.uk>
2017-12-20 18:53:19 +0000

    Clean up comments in general_weapon_functions.py

--- gear/weapons/general_weapon_functions.py
+++ gear/weapons/general_weapon_functions.py
@@ -14,36 +14,18 @@ def choose_weapon_element():
 
 def is_general_weapon_element_combo_valid(weapon_type, weapon_element):
 
-    # I think the only universal limitation for all manufacturers is that
-    # smg's and sniper rifles CANNOT be explosive
-
-    # Oh, and also that all launchers must be elemental, you can't have
-    # a non-elemental launcher I don't think?
-
-
-    # Lets set these True by default
+    # The only universal limitations on weapon-element combos for all
+    # manufacturers is that smg's and sniper rifle can't be explosive, and
+    # a launcher can't be non elemental
 
     test_1 = True
     test_2 = True
 
     if(weapon_element == "Explosion"):
-        # We get True if the weapon is a launcher or a shotgun, since
-        # the only explosive weapons allowed are launchers or shotguns
-
         test_1 = (weapon_type == "launcher" or weapon_type == 'shotgun' or weapon_type == 'pistol' or weapon_type == 'assault_rifle')
     
-
     if(weapon_type == 'launcher'):
         test_2 = (weapon_element != "None")
-
-    # Return the boolean of test_1 and test_2; if true then the weapon
-    # has passed both tests and is a valid GENERAL weapon, but we must
-    # still check the manufacturer specific requirements...
-
-    # That last comment makes me think that it may sometimes take quite
-    # a while to generate a valid weapon, eprhaps this effort should
-    # be going to creating rules that a valid weapon is chosen, rather
-    # than simply checking and then regenrating if the check fails?
 
     return test_1 and test_2
 
