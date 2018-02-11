76411ffbd96eaf3ae6c26c10c07c4cf150b0d2c5
Yousef Moazzam <mrbenzedrine@live.co.uk>
2018-02-11 00:37:56 +0000

    Edit for loop to not unnecessarily use the range function

--- gear/class_mods/general_class_mod_functions.py
+++ gear/class_mods/general_class_mod_functions.py
@@ -24,8 +24,8 @@ def get_remaining_skills_to_boost(rarity, com_prefix, fixed_skills_from_com_to_b
 
     # Add the skills in other_skills_to_boost to all_skills_to_boost
 
-    for x in range(0, len(other_skills_to_boost)):
+    for skill in other_skills_to_boost:
         # Use a placeholder value of 1 for now
-        all_skills_to_boost[other_skills_to_boost[x]] = 1
+        all_skills_to_boost[skill] = 1
 
     return all_skills_to_boost
