092f1760ac7d235854c472fce183e3c14d391d4d
Yousef Moazzam <mrbenzedrine@live.co.uk>
2018-03-04 17:45:32 +0000

    Remove unnecessary parentheses in if statements in general_weapon_functions.py

--- gear/weapons/general_weapon_functions.py
+++ gear/weapons/general_weapon_functions.py
@@ -20,10 +20,10 @@ def is_general_weapon_element_combo_valid(weapon_type, weapon_element):
     test_1 = True
     test_2 = True
 
-    if(weapon_element == "Explosion"):
+    if weapon_element == "Explosion":
         test_1 = (weapon_type == "launcher" or weapon_type == 'shotgun' or weapon_type == 'pistol' or weapon_type == 'assault_rifle')
     
-    if(weapon_type == 'launcher'):
+    if weapon_type == 'launcher':
         test_2 = (weapon_element != "None")
 
     return test_1 and test_2
@@ -35,7 +35,7 @@ def green_blue_rarity_spawn_with_accessory():
 
     spawn_with_accesory_chance = random.randint(0,2)
 
-    if(spawn_with_accesory_chance == 0):
+    if spawn_with_accesory_chance == 0:
         spawn_with_accesory = True
     else:
         spawn_with_accesory = False
@@ -46,7 +46,7 @@ def is_rarity_element_combo_valid(rarity, element):
 
     # E-Tech rarity weapons cannot be non-elemental or explosive
 
-    if(rarity != 'E-Tech'):
+    if rarity != 'E-Tech':
         is_valid = True
     else:
         is_valid = (element != 'None' and element != 'Explosion')
@@ -55,11 +55,11 @@ def is_rarity_element_combo_valid(rarity, element):
 
 def is_general_manufacturer_element_combo_valid(manufacturer, element):
 
-    if(manufacturer == 'Torgue'):
+    if manufacturer == 'Torgue':
         is_valid = element == 'Explosion'
-    elif(manufacturer == 'Maliwan'):
+    elif manufacturer == 'Maliwan':
         is_valid = (element != 'None' and element != 'Explosion')
-    elif(manufacturer == 'Jakobs'):
+    elif manufacturer == 'Jakobs':
         is_valid = element == 'None'
     else:
         is_valid = element != 'Explosion'
