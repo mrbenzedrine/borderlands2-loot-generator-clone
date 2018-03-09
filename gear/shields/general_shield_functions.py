e90c535d9289226f3183c151e43e3152a5ccc03f
Yousef Moazzam <mrbenzedrine@live.co.uk>
2018-03-09 22:59:03 +0000

    Remove unnecessary parentheses in if statements in general_shield_functions.py

--- gear/shields/general_shield_functions.py
+++ gear/shields/general_shield_functions.py
@@ -7,7 +7,7 @@ def choose_element():
 def get_valid_nova_spike_manufacturer_element_combo(manufacturer, element, type):
     generated_element = element #set generated_element to be the same as element for now
     while True:
-        if(is_nova_spike_manufacturer_element_combo_valid(manufacturer, generated_element) is True):
+        if is_nova_spike_manufacturer_element_combo_valid(manufacturer, generated_element) is True:
             print("Valid %s element/manufacturer combo" % type)
             print("%s %s shield has element %s" % (manufacturer, type, generated_element))
             break
@@ -25,9 +25,9 @@ def is_nova_spike_manufacturer_element_combo_valid(manufacturer, element):
     test_1 = True
     test_2 = True
 
-    if(manufacturer == 'Maliwan'):
+    if manufacturer == 'Maliwan':
         test_1 = (element != 'Explosion')
-    elif(manufacturer == 'Torgue'):
+    elif manufacturer == 'Torgue':
         test_2 = (element == 'Explosion')
 
     return test_1 and test_2
